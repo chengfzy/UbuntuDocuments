47c1150572e6b90d770cdc0f95e64de037955d79
Cheng.Cheng <Cheng.Cheng@ygomi.com>
2018-09-19 21:46:54 +0800

    add select config script of ssr for OSX

--- shadowsocks/select_config.py
+++ shadowsocks/select_config.py
@@ -1,31 +1,38 @@
 import os
+import sys
 import shutil
 import subprocess
 
+
 def select_copy_config():
-    config_path = "/home/jeffery/.local/share/shadowsocksr/config.json"
-    
+    if sys.platform == 'linux' or sys.platform == 'linux2':
+        # linux
+        config_path = '/home/jeffery/.local/share/shadowsocksr/config.json'
+    elif sys.platform == 'darwin':
+        # OSX
+        config_path = '/Users/test/.local/share/shadowsocksr/config.json'
+
     # get all the file names in current folder
     choice = []
     for file in os.listdir('.'):
         if file.endswith('.json'):
             choice.append(file)
-    
+
     # print choose
     print("choose config file:")
     for (i, file) in enumerate(choice):
         print('\t[{0}] {1}'.format(i + 1, file))
-    
-    
+
     # check input choose
-    selected = raw_input('Please Select Choose: ')
-    while not (selected.isdigit() and int(selected) >=0 and int(selected) <= len(choice)):
-        selected = raw_input('Input Error, Please Select Choose: ')
-    
+    selected = input('Please select choose: ')
+    while not selected.isdigit() or int(selected) < 0 or int(selected) > len(choice):
+        selected = input('Input error, please select choose: ')
+
     # copy file to folder
     selected = int(selected)
     shutil.copy('./' + choice[selected - 1], config_path)
-    
+
+
 # check sudo
 if __name__ == '__main__':
     if os.getuid() == 0:
