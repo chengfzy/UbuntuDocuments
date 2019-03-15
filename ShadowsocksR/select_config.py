d705099668ef3c1d13353fb01ff041f18b87df76
jeffer <chengcheng2006@163.com>
2019-03-15 21:58:37 +0800

    modify select ssr config

--- ShadowsocksR/select_config.py
+++ ShadowsocksR/select_config.py
@@ -4,7 +4,7 @@ import shutil
 import subprocess
 
 
-def select_copy_config():
+def select_copy_config(folder):
     if sys.platform == 'linux' or sys.platform == 'linux2':
         # linux
         config_path = '/home/jeffery/.local/share/shadowsocksr/config.json'
@@ -13,31 +13,32 @@ def select_copy_config():
         config_path = '/Users/test/.local/share/shadowsocksr/config.json'
 
     # get all the file names in current folder
-    choice = []
-    for file in os.listdir('.'):
+    choices = []
+    for file in os.listdir(folder):
         if file.endswith('.json'):
-            choice.append(file)
+            choices.append(file)
+    print(choices)
 
     # print choose
-    print("choose config file:")
-    for (i, file) in enumerate(choice):
+    print('choose config file:')
+    for (i, file) in enumerate(choices):
         print('\t[{0}] {1}'.format(i, file))
 
     # check input choose
     selected = input('Please select choose: ')
-    while not selected.isdigit() or int(selected) < 0 or int(selected) > len(choice):
+    while not selected.isdigit() or int(selected) < 0 or int(selected) > len(choices):
         selected = input('Input error, please select choose: ')
 
     # copy file to folder
     selected = int(selected)
-    shutil.copy('./' + choice[selected], config_path)
+    shutil.copy(os.path.join(folder, choices[selected]), config_path)
 
 
-# check sudo
 if __name__ == '__main__':
+    folder = os.path.dirname(os.path.realpath(__file__))
+    # check sudo
     if os.getuid() == 0:
-        select_copy_config()
-        subprocess.call('sudo ./ssr stop', shell=True)
-        subprocess.call('sudo ./ssr start', shell=True)
+        select_copy_config(folder)
+        subprocess.call('sudo ' + os.path.join(folder, 'ssr') + ' restart', shell=True)
     else:
         print('please add sudo to run the script')
